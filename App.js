b47a2449d91caf2be42becf83d7a4b7ea31774be
Carlos Jimenez <carlos_jmnz@hotmail.com>
2021-09-09 16:36:31 -0500

    mi codigo nuevo de la caja de texto

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {StyleSheet, View, Text} from 'react-native'
 
 export default function App() {
-  return (
+  return(
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, world!</Text>
+          <Text style={styles.text}>Texto en caja texto</Text>
     </View>
+
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
+const styles=StyleSheet.create({
+  container:{
     flex: 1,
     backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems:'center',
+    justifyContent:'center',
   },
-  text: {
+  text:{
     color: 'rgb(59,108,212)',
     fontSize: 42,
     fontWeight: '100',
     textAlign: 'center',
   },
+
 })
