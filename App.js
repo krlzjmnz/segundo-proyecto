15eaac3a5407f1e54e475bbb48628c36130b2917
Carlos Jimenez <carlos_jmnz@hotmail.com>
2021-10-25 14:55:33 -0500

    boton editado

--- App.js
+++ App.js
@@ -1,31 +1,40 @@
-import React, {useState} from 'react'
-import {Button, StyleSheet, Text, View} from 'react-native'
+import React {useState} from 'react'
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native'
 
 export default function App(){
-    const [count, setCount] =useState(0)
+  const [count, setCount] = useState(0)
 
-    return (
-        <View style={styles.container}>
-            <Button
-                title={'Oprime me'}
-                onPress={()=>{
-                    setCount(count+1)
-                }}
-            />
-            <Text style={styles.text}>{`Pressed ${count} times`}</Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+    <TouchableOpacity
+    style={styles.button}
+    activeOpacity={0.7}
+    onPress={()=>{
+      setCount(count+1)
+    }}
+    >
+    <Text style={styles.text}>press me!</Text>
+    </TouchableOpacity>
+    <Text style={styles.text}>{`pressed ${count} times`}</Text>
+    </View>
+  )
 }
 
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems: 'center',
-        justifyContent:'center',
-    },
-    text:{
-        fontSize: 18,
-        padding:12,
-    },
-
+const styles=StyleSheet.creeate({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifycontent:'center'
+  },
+  button:{
+    padding:40,
+    borderRadius:4,
+    borderWidth:1,
+    boderColor:'green',
+    backgroundColor:'lightgreen',
+  },
+  text:{
+    fontSize:18,
+    padding:12,
+  },
 })
